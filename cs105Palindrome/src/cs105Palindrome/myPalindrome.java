54eb6034a1f25a51d5e7b7e0514b3beaa290db03
Andre Basson <basson.andre@gmail.com>
2016-07-27 16:40:53 +1200

    first commit, master

---
+++ cs105Palindrome/src/cs105Palindrome/myPalindrome.java
@@ -0,0 +1,22 @@
+package cs105Palindrome;
+
+import java.util.Stack;
+
+public class myPalindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		myPalindrome prog = new myPalindrome();	// default constructor called
+		
+		Stack<Character> s1 = new Stack<Character>();
+		Stack<Character> s2 = new Stack<Character>();
+		
+		
+
+	}
+	
+	private boolean checkPalindrome(Stack<Character> s1, Stack<Character> s2){
+		return true; 	// place holder
+	}
+
+}
